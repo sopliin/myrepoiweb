3b2bd0d4e908519e3bc4235f61621c7772f1e0dd
Rodrigo <rrojass@pucp.edu.pe>
2025-04-18 01:23:22 -0500

    creacion y edicion de ClasePrueba.java

---
+++ ClasePrueba.java
@@ -0,0 +1,17 @@
+public class ClasePrueba
+{
+    //Atributos de la clase
+    private String atributo1;
+    private int atributo2;
+    private float atributo3;
+
+    //Constructor con el mismo nombre de la clase
+    public MiClase(){}
+
+    //Métodos de la clase
+    public void metodo1()
+    {
+        //Método vacío
+    }
+
+}
